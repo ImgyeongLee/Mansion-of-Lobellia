11d28ee4828142307c2ab7d87218f1a3ae7e6a06
hlakarki <kshap.karki@gmail.com>
2025-01-09 17:23:05 -0500

    feat: Added database query function to retrieve battle participants and their associated skills

---
+++ src/lib/db/actions/lobby.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/db/prisma";
+
+export async function getParticipantsInfo(battleId: string) {
+  try {
+    const battleInfo = await prisma.battleRoom.findUnique({
+      where: {
+        id: battleId,
+      },
+      include: {
+        participants: {
+          include: {
+            skills: {
+              include: {
+                skill: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!battleInfo) return { success: false, error: "Battle not found" }
+
+    return { success: true, battleInfo };
+  } catch (error) {
+    return { success: false, error };
+  }
+}
