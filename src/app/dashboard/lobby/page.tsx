105d39eb0ae2e9d1942ffbea00c57850eb020066
hlakarki <kshap.karki@gmail.com>
2025-01-09 17:49:11 -0500

    feat: Added battle info display and imported getUserDungeonsWithInfo function to lobby page, enhanced party lobby section with dynamic data rendering

--- src/app/dashboard/lobby/page.tsx
+++ src/app/dashboard/lobby/page.tsx
@@ -5,6 +5,7 @@ import { getCurrentUser } from "aws-amplify/auth/server";
 import prisma from "@/lib/db/prisma";
 import { getSub } from "@/lib/db/actions/cookies";
 import { RedirectWithToast } from "@/app/_components/redirectToast";
+import {getUserDungeonsWithInfo} from "@/lib/db/actions/lobby";
 
 export default async function LobbyPage() {
   let currentUserSub = await getSub();
@@ -27,6 +28,8 @@ export default async function LobbyPage() {
     },
   });
 
+  const battleInfo = await getUserDungeonsWithInfo(currentUserSub)
+
   if (!character) {
     return (
       <RedirectWithToast
@@ -42,7 +45,12 @@ export default async function LobbyPage() {
     <section className="grid grid-cols-[1fr_5fr_1fr] w-full h-full">
       <div className="col-start-2 flex flex-col h-full">
         <div className="flex flex-row mt-16 mb-14 justify-between items-center">
-          <div className="text-4xl">Party Lobby</div>
+          <div className="text-4xl">
+              <h3>Party Lobby</h3>
+              {battleInfo.data && (
+                  <div className={"text-sm"}>{JSON.stringify(battleInfo.data)}</div>
+              )}
+          </div>
         </div>
         <div className="grid grid-rows-3 items-center h-full -mt-10 mb-6">
           {!character.roomId && (
