37d0e979efdcda8738298e90679dfcb6e9430b64
hlakarki <kshap.karki@gmail.com>
2025-01-09 17:48:36 -0500

    refactor: Replaced getBattleInfo with getUserDungeonsWithInfo to fetch user's battle rooms and associated character information from Prisma

--- src/lib/db/actions/lobby.ts
+++ src/lib/db/actions/lobby.ts
@@ -1,28 +1,60 @@
 import prisma from "@/lib/db/prisma";
 
-export async function getBattleInfo(battleId: string) {
+export async function getUserDungeonsWithInfo(userId: string) {
   try {
-    const battleInfo = await prisma.battleRoom.findUnique({
+    const userBattles = await prisma.character.findUnique({
       where: {
-        id: battleId,
+        userId: userId,
       },
-      include: {
-        participants: {
+      select: {
+        battleRoom: {
           include: {
-            skills: {
-              include: {
-                skill: true,
-              },
+            participants: {
+                include: {
+                    skills: {
+                        include: {
+                            skill: true,
+                        },
+                    },
+                },
             },
+            entities: true,
           },
         },
       },
     });
 
-    if (!battleInfo) return { success: false, error: "Battle not found" }
+    if (!userBattles) return { success: false, error: "Character not found" };
 
-    return { success: true, battleInfo };
+    return { success: true, data: userBattles.battleRoom };
   } catch (error) {
     return { success: false, error };
   }
 }
+
+// export async function getBattleInfo(battleId: string) {
+//     try {
+//         const battleInfo = await prisma.battleRoom.findUnique({
+//             where: {
+//                 id: battleId,
+//             },
+//             include: {
+//                 participants: {
+//                     include: {
+//                         skills: {
+//                             include: {
+//                                 skill: true,
+//                             },
+//                         },
+//                     },
+//                 },
+//             },
+//         });
+//
+//         if (!battleInfo) return { success: false, error: "Battle not found" };
+//
+//         return { success: true, battleInfo };
+//     } catch (error) {
+//         return { success: false, error };
+//     }
+// }
